559b4988eefc545fb22feea72a4da22b883592b4
NIMISHA RAMESH <164715535+NimishaRamesh21@users.noreply.github.com>
2024-11-23 16:27:18 +0530

    Add files via upload

---
+++ career/college.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Display the success message after 2 seconds
+    setTimeout(function() {
+        document.getElementById("successMessage").style.display = "block";
+    }, 2000);
+});
